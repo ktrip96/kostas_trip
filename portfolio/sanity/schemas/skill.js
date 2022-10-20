b97a6f0fa31dcc71224b4bb89c616c7c22c9e1a7
ktrip96 <ktrip96@gmail.com>
2022-10-21 01:09:17 +0300

    Sanity set up

---
+++ portfolio/sanity/schemas/skill.js
@@ -0,0 +1,21 @@
+export default {
+  name: 'skill',
+  title: 'Skill',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      description: 'Title of skill',
+      type: 'string',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+}
---
+++ portfolio/sanity/schemas/social.js
@@ -0,0 +1,18 @@
+export default {
+  name: 'social',
+  title: 'Social',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'title',
+      description: 'platform for social media',
+      type: 'string',
+    },
+    {
+      name: 'url',
+      title: 'Url',
+      type: 'url',
+    },
+  ],
+}
